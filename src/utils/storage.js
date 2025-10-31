41d483d360e3cd97c2b5d953105da6407037f7ae
SulthanRaghib <raghib.smp@gmail.com>
2025-10-31 16:34:59 +0700

    feat: add storage utility functions for localStorage management

---
+++ src/utils/storage.js
@@ -0,0 +1,16 @@
+export const saveData = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+export const getData = (key) => {
+  const data = localStorage.getItem(key);
+  return data ? JSON.parse(data) : null;
+};
+
+export const removeData = (key) => {
+  localStorage.removeItem(key);
+};
+
+export const clearAll = () => {
+  localStorage.clear();
+};
